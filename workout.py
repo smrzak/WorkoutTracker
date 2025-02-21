777020e8a43d65fe6bebdc3b26e922a95aa5cce0
Jakub <smrzja1@gmail.com>
2025-02-21 06:58:38 +0100

    Switched to CSV logging

--- workout.py
+++ workout.py
@@ -1,16 +1,30 @@
+import csv  
+
 def log_workout():
     exercise = input("Enter exercise name: ")
     sets = input("Enter number of sets: ")
     reps = input("Enter number of reps: ")
     weight = input("Enter weight (kg): ")
     
-    with open("workouts.txt", "a") as file:
-        file.write(f"{exercise}, {sets} sets, {reps} reps, {weight} kg\n")
+    with open("workouts.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([exercise, sets, reps, weight])
     
     print("Workout logged!")
+
+try:
+    with open("workouts.csv", "r"):
+        pass  
+except FileNotFoundError:
+    with open("workouts.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["exercise", "sets", "reps", "weight"])
 
 while True:
     log_workout()
     again = input("Log another workout? (y/n): ")
     if again.lower() != "y":
         break
+
+
+#https://github.com/smrzak/WorkoutTracker.git
